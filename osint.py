5d4819f649986fb0f408711bdc79c9e0613cfdda
GyanPrakash2483 <gyanprakash2483@gmail.com>
2025-06-21 16:35:32 +0530

    Flipkart support

--- osint.py
+++ osint.py
@@ -1,5 +1,5 @@
 import asyncio
-from modules import facebook, instagram, google, microsoft, snapchat, twitter, yahoo, amazon
+from modules import facebook, instagram, google, microsoft, snapchat, twitter, yahoo, amazon, flipkart
 
 async def main(email):
     print(f"\n🔍 Checking OSINT email linkage for: {email}\n")
@@ -11,6 +11,7 @@ async def main(email):
     await twitter.check(email)
     await yahoo.check(email)
     await amazon.check(email)
+    await flipkart.check(email)
 
 if __name__ == "__main__":
     import sys
