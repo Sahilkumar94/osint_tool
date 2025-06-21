19cbbd0e6b1464387035abec4a8a277be736af1b
Sahil <124199168+Sahilkumar94@users.noreply.github.com>
2025-06-21 12:44:26 +0530

    Add files via upload

---
+++ osint.py
@@ -0,0 +1,19 @@
+import asyncio
+from modules import facebook, instagram, google, microsoft, snapchat, twitter, yahoo
+
+async def main(email):
+    print(f"\n🔍 Checking OSINT email linkage for: {email}\n")
+    await facebook.check(email)
+    await instagram.check(email)
+    await google.check(email)
+    await microsoft.check(email)
+    await snapchat.check(email)
+    await twitter.check(email)
+    await yahoo.check(email)
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python check_email_all.py <email>")
+    else:
+        asyncio.run(main(sys.argv[1]))
